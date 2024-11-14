6c7fad2d816fb6937cb1a4e3655b1281c94a6746
Dhriti Ankapalli <dhritia04@gmail.com>
2024-11-14 11:27:04 +0530

    Add files via upload

---
+++ recipe-app-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RecipeApp from './components/RecipeApp'; // Correct import path
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeApp /> {/* Use the RecipeApp component here */}
+    </div>
+  );
+}
+
+export default App;
